3938d9b571226e86342a71ec55742e5478c62d29
n0h0 <kusakatsubasa@gmail.com>
2023-01-02 21:35:41 +0900

    Setup CI

--- spec/acclir_spec.rb
+++ spec/acclir_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Acclir do
   it "has a version number" do
     expect(Acclir::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
